62fa388a37f6793b63a2248d5268fd20e006cbd2
ertrzyiks <mateusz.derks@gmail.com>
2018-03-04 18:22:33 +0100

    Add basic version of plugin

---
+++ index.js
@@ -0,0 +1,9 @@
+var generateResponsiveImages = require('../lib/responsive_images')
+var getNewPath = require('../lib/new_path')
+
+hexo.extend.helper.register('image_version', function (original, options) {
+  return getNewPath(original, options)
+});
+
+
+hexo.extend.filter.register('after_generate', generateResponsiveImages)
